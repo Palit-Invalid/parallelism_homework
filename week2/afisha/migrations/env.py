05c29224368f2aae4f80bd323a82deffae3b7313
Dmitry Fateev <dimkaaaa1@gmail.com>
2026-08-18 18:09:07 +0000

    Ignore F405

--- week2/afisha/migrations/env.py
+++ week2/afisha/migrations/env.py
@@ -13,7 +13,7 @@ config.set_main_option("sqlalchemy.url", f"{app_config.db.url}?async_fallback=Tr
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-target_metadata = Base.metadata
+target_metadata = Base.metadata  # noqa: F405
 
 
 def run_migrations_offline() -> None:
